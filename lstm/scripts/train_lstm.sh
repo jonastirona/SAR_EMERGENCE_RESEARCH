417278954c711ce079f84d86bf5e48f7f61f431d
B3rK-3 <josbuz211@gmail.com>
2025-07-09 11:21:43 -0400

    script

--- lstm/scripts/train_lstm.sh
+++ lstm/scripts/train_lstm.sh
@@ -14,8 +14,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ebd@njit.edu
 
-module load wulver
-source /mmfs1/project/mx6/ebd/SAR_EMERGENCE_RESEARCH/sar/bin/activate
+module load wulver foss/2023b Python/3.11.5 CUDA/12.4
+module li
+echo 'Loading environment'
+cd /mmfs1/project/mx6/ebd/SAR_EMERGENCE_RESEARCH/
+source sar/bin/activate
+pip -V
+echo 'Done loading environment'
 
-cd /mmfs1/project/mx6/ebd/SAR_EMERGENCE_RESEARCH/lstm/
+cd lstm/
 python -u train_w_stats.py 12 4 110 5 140 500 0.0012 0.115
