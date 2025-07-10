e62550685a84be4e9f3a5021e4b4e703bb45451f
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2025-07-10 18:54:09 -0400

    .

---
+++ lstm/scripts/eval_lstm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=eval_lstm
+#SBATCH --partition=gpu
+#SBATCH --account=mx6
+#SBATCH --qos=low
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:2
+#SBATCH --time=08:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ebd@njit.edu
+
+module load wulver foss/2023b Python/3.11.5 CUDA/12.4
+module li
+echo 'Loading environment'
+cd /mmfs1/project/mx6/ebd/SAR_EMERGENCE_RESEARCH/
+source sar/bin/activate
+pip -V
+echo 'Done loading environment'
+
+cd lstm/
+python -u eval.py
