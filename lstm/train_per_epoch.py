aa17670f55d046d1b51cec1477cbdf6781fbd745
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2025-07-16 18:22:44 -0400

    .

--- lstm/train_per_epoch.py
+++ lstm/train_per_epoch.py
@@ -15,7 +15,67 @@ from ray import tune
 
 warnings.filterwarnings("ignore")
 
+path = "/mmfs1/project/mx6/ebd/"
 
+def load_data(ARs, size):
+        all_inputs = []
+        all_flux = []
+        for AR in ARs:
+            pm_and_int = np.load(
+                path
+                + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_pmdop{}_flat.npz".format(AR, AR),
+                allow_pickle=True,
+            )
+            mag_flux = np.load(
+                path
+                + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_mag{}_flat.npz".format(AR, AR),
+                allow_pickle=True,
+            )
+            intensities = np.load(
+                path
+                + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_int{}_flat.npz".format(AR, AR),
+                allow_pickle=True,
+            )
+            power_maps23 = pm_and_int["arr_0"]
+            power_maps34 = pm_and_int["arr_1"]
+            power_maps45 = pm_and_int["arr_2"]
+            power_maps56 = pm_and_int["arr_3"]
+            mag_flux = mag_flux["arr_0"]
+            intensities = intensities["arr_0"]
+            # Trim array to get rid of top and bottom 0 tiles
+            power_maps23 = power_maps23[rid_of_top * size : -rid_of_top * size, :]
+            power_maps34 = power_maps34[rid_of_top * size : -rid_of_top * size, :]
+            power_maps45 = power_maps45[rid_of_top * size : -rid_of_top * size, :]
+            power_maps56 = power_maps56[rid_of_top * size : -rid_of_top * size, :]
+            mag_flux = mag_flux[rid_of_top * size : -rid_of_top * size, :]
+            mag_flux[np.isnan(mag_flux)] = 0
+            intensities = intensities[rid_of_top * size : -rid_of_top * size, :]
+            intensities[np.isnan(intensities)] = 0
+            # stack inputs and normalize
+            stacked_maps = np.stack(
+                [power_maps23, power_maps34, power_maps45, power_maps56], axis=1
+            )
+            stacked_maps[np.isnan(stacked_maps)] = 0
+            min_p = np.min(stacked_maps)
+            max_p = np.max(stacked_maps)
+            min_m = np.min(mag_flux)
+            max_m = np.max(mag_flux)
+            min_i = np.min(intensities)
+            max_i = np.max(intensities)
+            stacked_maps = min_max_scaling(stacked_maps, min_p, max_p)
+            mag_flux = min_max_scaling(mag_flux, min_m, max_m)
+            intensities = min_max_scaling(intensities, min_i, max_i)
+            # Reshape mag_flux to have an extra dimension and then put it with pmaps
+            int_reshaped = np.expand_dims(intensities, axis=1)
+            pm_and_int = np.concatenate([stacked_maps, int_reshaped], axis=1)
+            # append all ARs
+            all_inputs.append(pm_and_int)
+            all_flux.append(mag_flux)
+        all_inputs = np.stack(all_inputs, axis=-1)
+        all_flux = np.stack(all_flux, axis=-1)
+        input_size = np.shape(all_inputs)[1]
+        return all_inputs, all_flux, input_size
+  
 def main(
     device,
     config,
@@ -28,7 +88,6 @@ def main(
     learning_rate,
     dropout,
 ):
-    path = "/mmfs1/project/mx6/ebd/"
 
     # Now you can use these variables in your script
     ARs = [
@@ -74,7 +133,6 @@ def main(
         13085,
         13098,
     ]
-    flatten = True
     size = 9
     tiles = size**2 - 2 * size * rid_of_top
     test_AR = 13179  # and the secondary will be 13165 and if I fix it, third: 13183
@@ -83,62 +141,9 @@ def main(
 
     # Preprocessing
     print("Load data and split in tiles for {} ARs".format(len(ARs)))
-    all_inputs = []
-    all_flux = []
-    for AR in ARs_:
-        pm_and_int = np.load(
-            path
-            + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_pmdop{}_flat.npz".format(AR, AR),
-            allow_pickle=True,
-        )
-        mag_flux = np.load(
-            path
-            + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_mag{}_flat.npz".format(AR, AR),
-            allow_pickle=True,
-        )
-        intensities = np.load(
-            path
-            + "SAR_EMERGENCE_RESEARCH/data/AR{}/mean_int{}_flat.npz".format(AR, AR),
-            allow_pickle=True,
-        )
-        power_maps23 = pm_and_int["arr_0"]
-        power_maps34 = pm_and_int["arr_1"]
-        power_maps45 = pm_and_int["arr_2"]
-        power_maps56 = pm_and_int["arr_3"]
-        mag_flux = mag_flux["arr_0"]
-        intensities = intensities["arr_0"]
-        # Trim array to get rid of top and bottom 0 tiles
-        power_maps23 = power_maps23[rid_of_top * size : -rid_of_top * size, :]
-        power_maps34 = power_maps34[rid_of_top * size : -rid_of_top * size, :]
-        power_maps45 = power_maps45[rid_of_top * size : -rid_of_top * size, :]
-        power_maps56 = power_maps56[rid_of_top * size : -rid_of_top * size, :]
-        mag_flux = mag_flux[rid_of_top * size : -rid_of_top * size, :]
-        mag_flux[np.isnan(mag_flux)] = 0
-        intensities = intensities[rid_of_top * size : -rid_of_top * size, :]
-        intensities[np.isnan(intensities)] = 0
-        # stack inputs and normalize
-        stacked_maps = np.stack(
-            [power_maps23, power_maps34, power_maps45, power_maps56], axis=1
-        )
-        stacked_maps[np.isnan(stacked_maps)] = 0
-        min_p = np.min(stacked_maps)
-        max_p = np.max(stacked_maps)
-        min_m = np.min(mag_flux)
-        max_m = np.max(mag_flux)
-        min_i = np.min(intensities)
-        max_i = np.max(intensities)
-        stacked_maps = min_max_scaling(stacked_maps, min_p, max_p)
-        mag_flux = min_max_scaling(mag_flux, min_m, max_m)
-        intensities = min_max_scaling(intensities, min_i, max_i)
-        # Reshape mag_flux to have an extra dimension and then put it with pmaps
-        int_reshaped = np.expand_dims(intensities, axis=1)
-        pm_and_int = np.concatenate([stacked_maps, int_reshaped], axis=1)
-        # append all ARs
-        all_inputs.append(pm_and_int)
-        all_flux.append(mag_flux)
-    all_inputs = np.stack(all_inputs, axis=-1)
-    all_flux = np.stack(all_flux, axis=-1)
-    input_size = np.shape(all_inputs)[1]
+    
+      
+    all_inputs,all_flux,input_size = load_data(ARs, size)
 
     # Start Training
     lstm = LSTM(input_size, hidden_size, num_layers, num_pred, dropout=dropout).to(
@@ -162,15 +167,22 @@ def main(
             # train slice
             X_tr, y_tr = lstm_ready(tile, size, pm_int, flux, num_in, num_pred)
             X_tr = torch.reshape(X_tr, (X_tr.shape[0], num_in, X_tr.shape[2]))
-
             X_train_list.append(X_tr)
             y_train_list.append(y_tr)
 
-            # test slice (you already use tiles//2 for your test)
-            X_te, y_te = lstm_ready(tiles // 2, size, pm_int, flux, num_in, num_pred)
+    ARs_ = [11190, 11462, 11521, 11907, 12219, 12271, 12275, 12567, 12571]
+    all_inputs,all_flux,input_size = load_data(ARs_, size)
+
+    for AR_ in range(len(ARs_)):
+        pm_int = all_inputs[:, :, :, AR_]
+        flux = all_flux[:, :, :, AR_] # Assuming flux also has this dimension
+        
+        for tile in range(tiles):
+            X_te, y_te = lstm_ready(tile, size, pm_int, flux, num_in, num_pred)
             X_te = torch.reshape(X_te, (X_te.shape[0], num_in, X_te.shape[2]))
             X_test_list.append(X_te)
             y_test_list.append(y_te)
+
     # concatenate
     X_train_all = torch.cat(X_train_list, dim=0).to(device)
     y_train_all = torch.cat(y_train_list, dim=0).to(device)
@@ -215,12 +227,20 @@ def main(
                 test_preds = lstm(x)
                 test_loss = loss_fn(test_preds, y)
                 test_losses.append(test_loss.item())
-        # avg_train_loss = float(np.mean(train_loss))
-        # avg_test_loss = float(np.mean(test_losses))
+        avg_train_loss = float(np.mean(train_loss))
+        avg_test_loss = float(np.mean(test_losses))
 
         learning_rate = scheduler.get_last_lr()[0]
         scheduler.step()
 
+        wandb.log(
+            {
+                "epoch": epoch,
+                "train_loss": avg_train_loss,
+                "test_loss": avg_test_loss,
+                "learning_rate": learning_rate,
+            }
+        )
 
     # Save the model weights
     model_name = "t{}_r{}_i{}_n{}_h{}_e{}_l{}_d{}.pth".format(
