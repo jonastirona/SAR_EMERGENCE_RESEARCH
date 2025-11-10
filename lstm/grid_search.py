ee5c5fa524fd5909f5361ee6aa0a48ae441a50e9
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2025-11-10 14:52:26 -0500

    all hyper parameter search auto

--- lstm/grid_search.py
+++ lstm/grid_search.py
@@ -322,7 +322,7 @@ if __name__ == "__main__":
     )
 
     # Set up the Tuner
-    ray.init(num_cpus=16, num_gpus=1, include_dashboard=False)
+    ray.init(num_cpus=32, num_gpus=1, include_dashboard=False)
     train_ref = ray.put(tensor_train)
     val_ref = ray.put(tensor_val)
     tuner = tune.Tuner(
@@ -332,7 +332,7 @@ if __name__ == "__main__":
                 train_ref=train_ref,
                 val_ref=val_ref,
             ),
-            {"gpu": 1 / 16},
+            {"gpu": 1 / 32},
         ),
         tune_config=tune.TuneConfig(
             num_samples=parse_args()[
