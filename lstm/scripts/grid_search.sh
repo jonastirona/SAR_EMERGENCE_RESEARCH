d2e56c4f2fe3cfede4b195e69e0634ed00166d20
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2025-11-10 15:27:31 -0500

    .

--- lstm/scripts/grid_search.sh
+++ lstm/scripts/grid_search.sh
@@ -17,7 +17,7 @@ sbatch <<EOT
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ebd@njit.edu
 
-module load wulver foss/2023b Python/3.11.5 CUDA/12.6.0
+module load wulver foss/2024a Python/3.12.3 CUDA/12.6.0
 module li
 echo 'Loading environment'
 cd /mmfs1/project/mx6/ebd/SAR_EMERGENCE_RESEARCH/
